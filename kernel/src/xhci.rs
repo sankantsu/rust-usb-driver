bb76440d927acf61b4587213b36846b2f9671967
Godai Takashina <azarashi.azarashi.a@gmail.com>
2025-01-23 22:27:24 +0900

    feat: Check USB Legacy Support Capability
    Traverse xHCI extended capability list and check
    if USB Legacy Support Capability exists.
    I didn't implemented actual pre-OS to OS handoff
    because my QEMU environment seems not supporting it.

--- kernel/src/xhci.rs
+++ kernel/src/xhci.rs
@@ -13,10 +13,55 @@ impl xhci::accessor::Mapper for IdentityMapper {
     }
 }
 
+struct XHC {
+    mmio_base: usize,
+    registers: xhci::Registers<IdentityMapper>,
+    mapper: IdentityMapper,
+}
+
+impl XHC {
+    pub fn new(mmio_base: usize) -> Self {
+        let mapper = IdentityMapper {};
+        let mut registers = unsafe { xhci::Registers::new(mmio_base, mapper.clone()) };
+        Self {
+            mmio_base,
+            registers,
+            mapper,
+        }
+    }
+    fn init(&mut self) {
+        self.request_xhc_ownership()
+            .unwrap_or_else(|s| crate::serial_println!("{}", s));
+    }
+    fn request_xhc_ownership(&mut self) -> Result<(), &'static str> {
+        let hccp = self.registers.capability.hccparams1.read_volatile();
+        let mut ext_caps = unsafe {
+            xhci::extended_capabilities::List::new(self.mmio_base, hccp, self.mapper.clone())
+        }
+        .unwrap();
+        let mut iter = (&mut ext_caps).into_iter();
+        let usblegsup = loop {
+            let cap = iter.next();
+            match cap {
+                Some(cap) => {
+                    if let xhci::ExtendedCapability::UsbLegacySupport(legsup) = cap.unwrap() {
+                        break Some(legsup);
+                    }
+                }
+                None => break None,
+            }
+        }
+        .ok_or("USB legacy support capability is not implemented.")?;
+
+        // In QEMU environment, USB legacy support capability does not seem to be supported.
+        // So we will postpone implementing BIOS to OS handoff until actually needed...
+        unimplemented!();
+    }
+}
+
 pub fn init(pci_addr: PCIAddress) {
-    let mapper = IdentityMapper {};
     let mmio_base = pci_addr.read_bar_64(0).unwrap() as usize;
     crate::serial_println!("mmio_base: {:x}", mmio_base);
-    let mut registers = unsafe { xhci::Registers::new(mmio_base, mapper.clone()) };
-    crate::serial_println!("Succesfully initialized xhci registers!");
+    let mut xhc = XHC::new(mmio_base);
+    xhc.init();
 }
